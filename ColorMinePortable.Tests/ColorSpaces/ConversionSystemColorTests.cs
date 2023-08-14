4aeec11c46f52c9138c840cf187a6769dcf73dea
Satoshi Nakamura <kamuxxx@hotmail.com>
2023-08-14 13:51:39 +0900

    System.Drawing.Color Conversion

---
+++ ColorMinePortable.Tests/ColorSpaces/ConversionSystemColorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ColorMine.ColorSpaces;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace ColorMine.Tests.ColorSpaces;
+
+[TestFixture]
+public class ConversionSystemColorTests
+{
+    private double diff = 0.01;
+
+    [Test]
+    public void SystemWhiteToRgb()
+    {
+        var knownColor = Color.White;
+        var expectedColor = new Rgb { R = 255, G = 255, B = 255, };
+
+        var conv = knownColor.To<Rgb>();
+        (conv.R - expectedColor.R < diff).IsTrue();
+        (conv.G - expectedColor.G < diff).IsTrue();
+        (conv.B - expectedColor.B < diff).IsTrue();
+    }
+    [Test]
+    public void SystemGoldenrodToRgb()
+    {
+        var knownColor = Color.FromArgb(218, 165, 32);
+        var expectedColor = new Rgb { R = 218, G = 165, B = 32, };
+
+        var conv = knownColor.To<Rgb>();
+        (conv.R - expectedColor.R < diff).IsTrue();
+        (conv.G - expectedColor.G < diff).IsTrue();
+        (conv.B - expectedColor.B < diff).IsTrue();
+    }
+    [Test]
+    public void SystemMaroonToRgb()
+    {
+        var knownColor = Color.Maroon;
+        var expectedColor = new Rgb { R = 128, G = 0, B = 0 };
+
+        var conv = knownColor.To<Rgb>();
+        (conv.R - expectedColor.R < diff).IsTrue();
+        (conv.G - expectedColor.G < diff).IsTrue();
+        (conv.B - expectedColor.B < diff).IsTrue();
+    }
+}
+
---
+++ ColorMinePortable/ColorSpaces/SystemColorExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace ColorMine.ColorSpaces;
+
+public static class SystemColorExtension
+{
+    /// <summary>
+    /// Convert System.Drawing.Color to any IColorSpace
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="color"></param>
+    /// <returns></returns>
+    public static T To<T>(this Color color) where T: IColorSpace, new()
+    {
+        var rgb = new Rgb {
+            R = color.R,
+            G = color.G,
+            B = color.B
+        };
+
+        return rgb.To<T>();
+    }
+
+    /// <summary>
+    /// Convert any IColorSpace to System.Drawing.Color
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns></returns>
+    public static Color ToSystemColor(this IColorSpace color)
+    {
+        var rgb = color.ToRgb();
+        return Color.FromArgb(
+            (int)Math.Round(rgb.R, 0),
+            (int)Math.Round(rgb.G, 0),
+            (int)Math.Round(rgb.B, 0)
+        );
+    }
+}
+
